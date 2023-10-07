fe2dfa5e4409438618a03a3706264487444df7d5
IgorFVasconcelos <igorfervasc@alu.ufc.br>
2023-10-07 19:28:03 -0300

    versao instavel, favor não passar para main

--- client.py
+++ client.py
@@ -5,35 +5,42 @@ nickname = input("Enter a nickname: ")
 stop_thread = False
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55123))
+client.connect(('127.0.0.1', 55124))
+
+def is_command(message, command):
+    
+    if message.startswith('/'):
+      if message.find(command) != -1:
+          return True
+      else:
+          return False
 
 def receive():
     while True:
+        global stop_thread
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024)
+            message = str(message, 'ascii')
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
                 message = client.recv(1024).decode('ascii')
                 print("All ready, type anything to the chat")
                 if message == 'REFUSE NICK':
                     print('Someone with that nickname is already connected, please send another one. Type anything to proceed\n')
-                    new_nickname = input("Enter a new nickname: ")
-                    while new_nickname == nickname:
-                        if nickname != new_nickname:
-                            client.send(nickname.encode('ascii'))
-                        else:
-                            print("Nickname must be diferent than the one you wrote previously")
-                            new_nickname = input("Enter a new nickname: ")
-
+                    client.close()
+                    stop_thread = True
                 
             elif message == 'REFUSE SIZE': 
                 print('Conection refused, too many users on the server')
                 client.close()
-                stopThread = True
+                stop_thread = True
                 
+            # elif message == 'OK':
+            #     pass
+            
             else:
                 print(message)
-        except:
+        except:     
             print("An error occurred!")
             client.close()
             break
@@ -44,7 +51,20 @@ def write():
             client.close()
             break
         
-        message = f'{nickname}: {input("")}'
+        text = input(f"LOCAL: {nickname}: ")
+        
+        # if is_command(text, '/NICK'):
+        # if is_command(text, '/NICK'):
+        #     if len(text) > 6 and text[6] == " ":
+        #         client.send(text.enconde('ascii'))
+            
+        # elif is_command(text, '/USUARIOS'):
+        
+        # elif is_command(text, '/SAIR'):
+            
+        # else:    
+            #message = f'{nickname}: {text}'
+        message = (f'{nickname}: {text}')
         client.send(message.encode('ascii'))
 
 
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 HOST = '127.0.0.1'
-PORT = 55123
+PORT = 55124
 
 server = socket.socket(socket.AF_INET)
 server.bind((HOST, PORT))
@@ -11,16 +11,36 @@ server.listen()
 clients = []
 nicknames = []
 
-# BROADCAST: Sends message to client
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+def is_command(message, command, size):
+    print(f'message to decode: {message}')
+    if '/' in message[0]:    
+        if command in message[0:10]:
+            return True
+
+# BROADCAST: Sends message to all clients
+def broadcast(message, client):
+    message = str(message, 'ascii')
+    for user in clients:
+        if user != client:
+            user.send(message.encode('ascii'))
 
 def handle(client):
     while True:
         try:
             message = client.recv(1024)
-            broadcast(message)
+            print(type(message))
+            message = str(message, 'ascii')
+
+            #/NICK new_nick
+            if is_command(message, '/NICK', 5):
+                new_nick = message[6:]
+                index = nicknames.index(nickname)
+                broadcast(f'{nickname} has changed to {new_nick}')
+                nickname = new_nick
+                nicknames[index] = nickname
+            
+            else:
+                broadcast(message, client)
         except:
             index = clients.index(client)
             # Remove client
@@ -28,7 +48,7 @@ def handle(client):
             client.close()
             # Remove nickname
             nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat'.encode('ascii'))
+            broadcast(f'{nickname} left the chat'.encode('ascii'), client)
             nicknames.remove(nickname)
             break
 
@@ -39,14 +59,16 @@ def receive():
         print(f"Connected with {str(address)}")
         
         #RECUSAR SE TIVERMOS MAIS DE 4 CLIENTES 
-        tamanho = len(clients)
-        if tamanho >= 1:
+        tamanho = 0
+        if tamanho >= 2:
             client.send("REFUSE SIZE".encode('ascii'))
             client.close()
             break    
         
         client.send("NICK".encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')    
+        nickname = client.recv(1024)
+        nickname = str(nickname, 'ascii')
+        
         
         #SOLICITAR NOVO NICK SE JÁ HOUVER UM USUÁRIO COM UM IGUAL
         if len(nicknames) > 0:
@@ -57,9 +79,11 @@ def receive():
                     
         nicknames.append(nickname)
         clients.append(client)
+        tamanho += 1
 
         print(f'Nickname of the client is {nickname}')
-        broadcast(f'{nickname} joined the chat|'.encode('ascii'))
+        join = f'{nickname} joined the chat|.'.encode('ascii')
+        broadcast(join , client)
         client.send('Connected to the server|'.encode('ascii'))
 
         # We need to process multiple messages at a time
